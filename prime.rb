2ed8b80b7cb9ee2304d8eb38506fcb667bcb9b9a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-24 17:06:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
-def prime?(num)
-  num = 0
-  while num < 0
-    return false if num % n == 0
-    n += 1
-  end
-  true
-end
+# def prime?(num)
+#   num = 0
+#   while num < 0
+#     return false if num % n == 0
+#     n += 1
+#   end
+#   true
+# end
 
